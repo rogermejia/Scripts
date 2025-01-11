a5955df39a8db55fc3234ef999ab417e014ca29f
Rogelio Mejia <50418293+rogermejia@users.noreply.github.com>
2025-01-10 23:19:53 -0600

    Update CheckTime.js

--- CheckTime.js
+++ CheckTime.js
@@ -13,3 +13,14 @@ function checkTime(targetTimeUTC) {
 
 const targetTimeUTC = "20:00:00"; 
 checkTime(targetTimeUTC);
+
+
+
+function convertToUTC(localTime) {const localHours = parseInt(localTime.split(":")[0]); const localMinutes = parseInt(localTime.split(":")[1]); const localSeconds = parseInt(localTime.split(":")[2]); const utcHours = (localHours + 6) % 24; const utcTime = `${String(utcHours).padStart(2, '0')}:${String(localMinutes).padStart(2, '0')}:${String(localSeconds).padStart(2, '0')}`; return utcTime;} function checkTime(localTime) { const targetTimeUTC = convertToUTC(localTime); const interval = 1000; const checkInterval = setInterval(() => {const nowUTC = new Date().toUTCString(); const nowUTCTime = nowUTC.split(" ")[4]; 
+if (nowUTCTime >= targetTimeUTC) {
+    document.getElementById("demo").value = "";
+    object.method();
+    setTimeout(function(){
+        document.getElementById("demo").value = "";
+        object.method();
+    }, 5000); clearInterval(checkInterval);}}, interval);} const localTime = "23:16:00"; checkTime(localTime);
