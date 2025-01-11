b4d6de05e81f65675fb04ab1cefd8f6885c163d7
Rogelio Mejia <50418293+rogermejia@users.noreply.github.com>
2025-01-10 22:44:55 -0600

    Update CheckTime.js

--- CheckTime.js
+++ CheckTime.js
@@ -2,15 +2,14 @@ function checkTime(targetTimeUTC) {
     const interval = 1000; // Check every second
     const checkInterval = setInterval(() => {
         const nowUTC = new Date().toUTCString();
-        const nowUTCTime = nowUTC.split(" ")[4]; // Get the current time in HH:MM:SS format
+        const nowUTCTime = nowUTC.split(" ")[4]; 
 
         if (nowUTCTime >= targetTimeUTC) {
             console.log("Target time reached or passed:", targetTimeUTC);
-            clearInterval(checkInterval); // Stop checking once the target time is reached or passed
+            clearInterval(checkInterval); 
         }
     }, interval);
 }
 
-// Usage example
-const targetTimeUTC = "20:00:00"; // Set your target time in HH:MM:SS format
+const targetTimeUTC = "20:00:00"; 
 checkTime(targetTimeUTC);
