41d98a74a0668a2a51f8801fdf20135f84aeee5f
Rogelio Mejia <50418293+rogermejia@users.noreply.github.com>
2025-01-14 21:41:51 -0600

    Update CheckTime.js

--- CheckTime.js
+++ CheckTime.js
@@ -24,3 +24,14 @@ if (nowUTCTime >= targetTimeUTC) {
         document.getElementById("demo").value = "";
         object.method();
     }, 5000); clearInterval(checkInterval);}}, interval);} const localTime = "23:16:00"; checkTime(localTime);
+
+
+function convertToUTC(localTime) {const localHours = parseInt(localTime.split(":")[0]); const localMinutes = parseInt(localTime.split(":")[1]); 
+    const localSeconds = parseInt(localTime.split(":")[2]); const utcHours = (localHours + 6) % 24; 
+    const utcTime = `${String(utcHours).padStart(2, '0')}:${String(localMinutes).padStart(2, '0')}:${String(localSeconds).padStart(2, '0')}`; return utcTime;} 
+function checkTime(localTime, who) { const targetTimeUTC = convertToUTC(localTime); const interval = 1000; 
+    const checkInterval = setInterval(() => {const nowUTC = new Date().toUTCString(); const nowUTCTime = nowUTC.split(" ")[4]; if (nowUTCTime >= targetTimeUTC) { 
+    /*document.getElementById("demo").value = who; magicMethod();*/ 
+    console.log("mode: "+who+" at: "+localTime); clearInterval(checkInterval);}}, interval);} 
+const localTime = "23:16:00"; const endlocalTime = "23:16:00"; checkTime(localTime, "IN"); checkTime(endlocalTime, "OUT");
+
